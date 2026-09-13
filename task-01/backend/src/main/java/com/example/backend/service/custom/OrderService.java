5c4f88da884f1241a8a4f75c373c3365e7c76170
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-09-13 09:54:39 +0530

    feat(service): define order service interface

---
+++ task-01/backend/src/main/java/com/example/backend/service/custom/OrderService.java
@@ -0,0 +1,12 @@
+package com.example.backend.service.custom;
+
+import com.example.backend.dto.OrderRequestDTO;
+import com.example.backend.dto.OrderResposeDTO;
+import com.example.backend.dto.PaymentRequestDTO;
+
+public interface OrderService {
+    OrderResposeDTO createOrder(OrderRequestDTO requestDTO);
+    void processPayment(PaymentRequestDTO paymentRequestDTO);
+    void releaseExpiredReservations();
+    void cancelOrder(Long orderId);
+}
