abfe6152c1ff88ee4773589d5aafec15173b7be8
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-09-13 09:54:31 +0530

    feat(dto): create error response dto for global exceptions

---
+++ task-01/backend/src/main/java/com/example/backend/dto/ErrorResponseDTO.java
@@ -0,0 +1,17 @@
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDTO {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+}
