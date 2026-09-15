5263669fbc2808692dd11e16519a232e55374792
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-09-15 09:39:33 +0530

    feat(frontend): add types.ts

---
+++ task-02/frontend/src/types.ts
@@ -0,0 +1,28 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  images: string[];
+  category: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface OrderItem {
+  id: string;
+  orderId: string;
+  productId: string;
+  quantity: number;
+  price: number;
+}
+
+export interface Order {
+  id: string;
+  totalPrice: number;
+  status: 'RESERVED' | 'PAID' | 'EXPIRED' | 'FAILED';
+  createdAt: string;
+  updatedAt: string;
+  items: OrderItem[];
+}
